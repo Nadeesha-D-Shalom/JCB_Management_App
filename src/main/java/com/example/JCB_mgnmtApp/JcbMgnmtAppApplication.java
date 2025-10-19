10ea945ed5472317607e7f35099ea549f6a8d1f6
Nadeesha-D-Shalom <nadeeshashalom1@gmail.com>
2025-10-20 00:49:31 +0530

    Version 1.0.1 -Make and add classes - Add the main class

---
+++ src/main/java/com/example/JCB_mgnmtApp/JcbMgnmtAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.JCB_mgnmtApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcbMgnmtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcbMgnmtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JCB_mgnmtApp/JcbMgnmtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JCB_mgnmtApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcbMgnmtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
